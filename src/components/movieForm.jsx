4674365fff738e0a738c69932fe4ea1efbad5f57
maryamsp12 <maryamsp12@gmail.com>
2019-01-15 08:45:43 +0200

    initial commit

---
+++ src/components/movieForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MovieForm extends Component {
+    
+    render() { 
+        return ( 
+            <h1>Movie Form</h1>
+         );
+    }
+}
+ 
+export default MovieForm;
