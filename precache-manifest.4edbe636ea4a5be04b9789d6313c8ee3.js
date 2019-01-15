2731d5e28be2e67625183f38bd41f83e07317511
maryamsp12 <maryamsp12@gmail.com>
2019-01-15 17:27:39 +0200

    Updates

---
+++ precache-manifest.4edbe636ea4a5be04b9789d6313c8ee3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/online-movie-store/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1c3ead5b40d9de5510a1",
+    "url": "/online-movie-store/static/css/main.acfd1757.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/online-movie-store/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a01d8da8c7ada667c4df",
+    "url": "/online-movie-store/static/js/1.a01d8da8.chunk.js"
+  },
+  {
+    "revision": "4f83bf001716cb3c8c39",
+    "url": "/online-movie-store/static/js/runtime~main.4f83bf00.js"
+  },
+  {
+    "revision": "1c3ead5b40d9de5510a1",
+    "url": "/online-movie-store/static/js/main.1c3ead5b.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/online-movie-store/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/online-movie-store/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/online-movie-store/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a01d8da8c7ada667c4df",
+    "url": "/online-movie-store/static/css/1.e1a95244.chunk.css"
+  },
+  {
+    "revision": "9ce6f4997276909dfc762bd9ec427f0f",
+    "url": "/online-movie-store/index.html"
+  }
+];
