0025c78f68600eedf82fb42c8044f29421470eba
maryamsp12 <maryamsp12@gmail.com>
2019-01-15 17:07:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import { Route, Redirect, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import Movies from './components/movies'
 import Customers from './components/customers'
 import Rentals from './components/rentals'
 import NotFound from './components/notFound'
 import NavBar from './components/navbar'
 import './App.css';
-
-
 
 
 class App extends Component {
@@ -21,8 +19,7 @@ class App extends Component {
               <Route path="/customers" component={Customers}/>
               <Route path="/rentals" component={Rentals}/>
               <Route path="/not-found" component={NotFound}/>
-              <Redirect from="/" exact to="/movies" />
-              <Redirect to="/not-found" />
+              
               </Switch>
       </main>
       </React.Fragment>
