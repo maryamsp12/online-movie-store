26107a432ec554c4e5fcec23d3ca8a3599e71395
Maryam Tufail <maryamsp12@gmail.com>
2019-02-14 17:13:10 +0200

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ class NavBar extends Component {
         return ( 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
             
-            <Link className="navbar-brand" to="//movies">Vidly</Link>
+            <Link className="navbar-brand" to="/movies">Vidly</Link>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span></button>
